af874de00d6a4186640a8ce8d5b2c05e0354e2a5
Greg <GregJieNing@gmail.com>
2021-10-25 00:55:16 +0800

    init vue

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,25 +4,27 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src :self, :https, :data
+  policy.object_src :none
+  policy.form_action :self
+  policy.manifest_src :self
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+  if Rails.env.development?
+    policy.connect_src :self, "https://#{ViteRuby.config.host_with_port}", "ws://#{ViteRuby.config.host_with_port}"
+    policy.script_src :self, :unsafe_eval, "http://#{ViteRuby.config.host_with_port}", :unsafe_inline, 'https://polyfill.io'
+    policy.script_src_elem :self, :unsafe_inline
+    policy.style_src_elem  :self, :unsafe_inline
+    # policy.connect_src *policy.connect_src, :self
+  else
+    policy.connect_src :self
+    policy.script_src :self, :blob
+  end
 
-# If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+  # @inertiajs/progress uses inline styles
+  policy.style_src :self, :unsafe_inline
 
-# Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+  policy.base_uri :self
+end
