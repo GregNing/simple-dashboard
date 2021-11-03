1526e167db443aae5b633d1dc4432f9f8d47741b
Greg <GregJieNing@gmail.com>
2021-11-03 17:58:54 +0800

    use gmail

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -2,10 +2,9 @@
 
 ActionMailer::Base.smtp_settings = {
   port:      587,
-  address:   ENV['MAILGUN_SMTP_SERVER'],
-  user_name: ENV['MAILGUN_SMTP_LOGIN'],
-  password:  ENV['MAILGUN_SMTP_PASSWORD'],
-  domain: ENV['MAILGUN_DOMAIN'],
-  authentication: 'plain',
-  enable_starttls_auto: true
+  address:   ENV['SMTP_SERVER'],
+  user_name: ENV['SMTP_LOGIN'],
+  password:  ENV['SMTP_PASSWORD'],
+  domain: ENV['SMTP_DOMAIN'],
+  authentication: 'plain'
 }
